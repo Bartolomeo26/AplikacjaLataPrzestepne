de8d2b7cce4a0427ac87fe006877fe17761f63d2
Bartosz Spiżarny <83046@student.pb.edu.pl>
2023-05-22 18:40:27 +0200

    Paginated list not fully working

---
+++ AplikacjaLataPrzestepne/Pages/List.cshtml.cs
@@ -0,0 +1,52 @@
+using AplikacjaLataPrzestepne.Data;
+using AplikacjaLataPrzestepne.Forms;
+using ContosoUniversity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace AplikacjaLataPrzestepne.Pages
+{
+    public class ListModel : PageModel
+    {
+        public IEnumerable<RokPrzestepny> LeapYearList;
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
+        private readonly Wyszukiwania _context;
+
+        public ListModel(ILogger<IndexModel> logger, Wyszukiwania context, IConfiguration configuration)
+        {
+            _logger = logger;
+            _context = context;
+            Configuration = configuration;
+        }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public PaginatedList<RokPrzestepny> LeapYears_ { get; set; }
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
+        {
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            IQueryable<RokPrzestepny> studentsIQ = from s in _context.LeapData
+                                             select s;
+            var pageSize = Configuration.GetValue("PageSize", 20);
+            LeapYears_ = await PaginatedList<RokPrzestepny>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+        }
+            
+    }
+}
